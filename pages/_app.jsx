ae64ac5fc9d80354cc5188ddccccef4169e1a11d
Leonardo <leonardoleocabral@outlook.com>
2024-05-04 18:42:34 -0300

    fix(css): correct width

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,8 +7,8 @@ const NoSSR = dynamic(() => import('./index.jsx'), { ssr: false })
 
 export default function App() {
     return (
-        <div className='flex flex-col gap-1 text-center bg-green-50 mb-12'>
-            <header className='text-center py-7 bg-gradient-to-r from-green-600 to-green-900 shadow-md shadow-cyan-300 mb-6'>
+        <div className='flex flex-col gap-1 text-center bg-green-50 mb-12 min-w-full'>
+            <header className='text-center py-7 bg-gradient-to-r from-green-600 to-green-900 shadow-md shadow-cyan-300 mb-6 min-w-full'>
                 <h1 className='uppercase text-4xl text-white font-bold'>
                 ideias ambientais
                 </h1>
